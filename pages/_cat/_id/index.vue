1f84a66e0ffd69b04e0b54ee0f78b7c8c6019989
lamkostal <lamkostal@gmail.com>
2021-04-01 22:05:07 +0300

    add color field logic in product

--- pages/_cat/_id/index.vue
+++ pages/_cat/_id/index.vue
@@ -2,44 +2,69 @@
   <main>
     <article class="product">
       <div class="images">
-      <img :src="singleProduct.imgsrc" :alt="singleProduct.desc">
+        <img :src="singleProduct.imgsrc" :alt="singleProduct.desc" />
       </div>
       <div class="product-details">
         <div>
-          <h1>{{singleProduct.title}}</h1>
-          <p>{{singleProduct.desc}}</p>
-          <div class="product-options"><span v-if="hasSize">Available sizes: </span><span class="size-options">{{hasSizeOptions}}</span></div>
+          <h1>{{ singleProduct.title }}</h1>
+          <p>{{ singleProduct.desc }}</p>
+          <div class="product-options">
+            <div class="h-flex">
+              <span v-if="hasSize">Available sizes: </span
+              ><span class="size-options">{{ hasSizeOptions }}</span>
+            </div>
+             <div class="v-flex">
+              <span v-if="hasSize">Available colors: </span >
+              <div>
+                <div class="color-box" v-for="(color,index) in productColorsArr" :style="{background:color.color}" :key="index">{{index + 1}}</div>
+              </div>
+            </div>
+          </div>
         </div>
         <div>
-          <span class="price" :class="{strikeline : singleProduct.sales}">€{{singleProduct.price}}</span>
-          <span class="price salesprice" v-if="singleProduct.sales">€{{finalPrice}}</span>
-          <button 
-          class="btn buy-button snipcart-add-item"
-          :data-item-id="singleProduct.title"
-                    :data-item-price="finalPrice"
-                    :data-item-url="singleProduct.url"
-                    :data-item-name="singleProduct.title"
-                    :data-item-image="singleProduct.imgsrc"
-                    :data-item-description="singleProduct.desc"
-                    :data-item-custom1-name="hasSize?'Size':false"
-                    :data-item-custom1-options="hasSize?hasSizeOptions:false">
-          <span>Add to cart</span>
-           <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="25"> <path d="M2 2h4v4h16v11H4V4H2V2zm4 13h14V8H6v7zm0 4h3v3H6v-3zm14 0h-3v3h3v-3z" fill="black"/> </svg>
+          <span class="price" :class="{ strikeline: singleProduct.sales }"
+            >€{{ singleProduct.price }}</span
+          >
+          <span class="price salesprice" v-if="singleProduct.sales"
+            >€{{ finalPrice }}</span
+          >
+          <button
+            class="btn buy-button snipcart-add-item"
+            :data-item-id="singleProduct.title"
+            :data-item-price="finalPrice"
+            :data-item-url="singleProduct.url"
+            :data-item-name="singleProduct.title"
+            :data-item-image="singleProduct.imgsrc"
+            :data-item-description="singleProduct.desc"
+            :data-item-custom1-name="hasSize ? 'Size' : false"
+            :data-item-custom1-options="hasSize ? hasSizeOptions : false"
+          >
+            <span>Add to cart</span>
+            <svg
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              height="25"
+            >
+              <path
+                d="M2 2h4v4h16v11H4V4H2V2zm4 13h14V8H6v7zm0 4h3v3H6v-3zm14 0h-3v3h3v-3z"
+                fill="black"
+              />
+            </svg>
           </button>
         </div>
       </div>
     </article>
-      
   </main>
 </template>
 
 <script>
 export default {
-   asyncData(context) {
+  asyncData(context) {
     return context.app.$storyapi
       .get("cdn/stories", {
         version: process.env.NODE_ENV == "production" ? "published" : "draft",
-        starts_with: `products/${context.params.cat}`
+        starts_with: `products/${context.params.cat}`,
       })
       .then((res) => {
         console.log(res);
@@ -49,71 +74,92 @@ export default {
               title: pr.content.name,
               desc: pr.content.description,
               price: pr.content.price,
-              imgsrc:pr.content.images[0].filename,
-              incat:pr.content.incategory,
-              catlist:pr.tag_list,
-              full_slug:pr.full_slug,
-              slug:pr.slug,
-              size:pr.content.size,
-              sales:pr.content.sales,
-              discount:pr.content.discount,
+              imgsrc: pr.content.images[0].filename,
+              incat: pr.content.incategory,
+              catlist: pr.tag_list,
+              full_slug: pr.full_slug,
+              slug: pr.slug,
+              size: pr.content.size,
+              sales: pr.content.sales,
+              discount: pr.content.discount,
+              color1: pr.content.color1,
+              color2: pr.content.color2,
+              
+
+
             };
           }),
         };
       })
       .catch((e) => console.log(e));
   },
-  computed:{
-    singleProduct(){
-      return this.products.find((e)=>e.slug===this.$route.params.id)
+  computed: {
+    singleProduct() {
+      return this.products.find((e) => e.slug === this.$route.params.id);
     },
-    hasSize(){
-        return this.singleProduct.size.length
+    productColorsArr(){
+      return Object.values(this.singleProduct).filter(item => Object.keys(item).includes('color') && item.color.length)
     },
-    hasSizeOptions(){
-       var size = this.singleProduct.size
-       var loop = ''
-       for (var i=0; i<size.length; i++){
-            loop+= size[i]+"|"}
-          var  result=loop.substring(0, loop.length - 1)
-       return result
-        },
-     salesPrice(){
-            return  this.singleProduct.price*(1-this.singleProduct.discount/100).toFixed(2)
-        },
-    finalPrice(){
-            return  this.singleProduct.sales? (this.singleProduct.price*(1-this.singleProduct.discount/100)).toFixed(2) : this.singleProduct.price
+    hasSize() {
+      return this.singleProduct.size.length;
+    },
+    hasSizeOptions() {
+      var size = this.singleProduct.size;
+      var loop = "";
+      for (var i = 0; i < size.length; i++) {
+        loop += size[i] + "|";
+      }
+      var result = loop.substring(0, loop.length - 1);
+      return result;
+    },
+    salesPrice() {
+      return (
+        this.singleProduct.price *
+        (1 - this.singleProduct.discount / 100).toFixed(2)
+      );
+    },
+    finalPrice() {
+      return this.singleProduct.sales
+        ? (
+            this.singleProduct.price *
+            (1 - this.singleProduct.discount / 100)
+          ).toFixed(2)
+        : this.singleProduct.price;
+    },
+  },
+  mounted(){
+    console.log(this.productColorsArr)
   }
-}}
+};
 </script>
 
 <style>
-.product{
+.product {
   max-width: 80%;
   margin: auto;
   display: flex;
   justify-content: center;
   padding: 4rem;
 }
-.product>*{
+.product > * {
   margin: 1rem;
-  flex:1
+  flex: 1;
 }
-.product .images{
-  border:1px solid var(--sec-text-color);
+.product .images {
+  border: 1px solid var(--sec-text-color);
   padding: 1rem;
   border-radius: 10px;
   max-width: 313px;
 }
-.product-details{
+.product-details {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
 }
-.product-details p{
+.product-details p {
   margin: 2em;
 }
-.product .btn{
+.product .btn {
   width: 100%;
   border: none;
   padding: 1rem;
@@ -121,36 +167,49 @@ export default {
   margin: 2em 0 0 0;
   cursor: pointer;
   font-weight: bold;
-  text-transform:uppercase;
+  text-transform: uppercase;
   transition: ease-out 0.2s all;
   display: flex;
   align-items: center;
   justify-content: center;
 }
-.btn:hover{
+.btn:hover {
   background: var(--main-accent-color);
   color: #fff;
-  fill:blanchedalmond
+  fill: blanchedalmond;
 }
-.btn svg{
-  margin:0 1em;
-  
-  }
-.product .price{
+.btn svg {
+  margin: 0 1em;
+}
+.product .price {
   font-size: 2.5em;
 }
-.salesprice{
+.salesprice {
   color: var(--danger-color);
   font-weight: bold;
 }
-.product-options{
+.product-options {
   margin: 1em;
 }
-.size-options{
+.size-options {
   font-size: 1.1em;
   font-weight: bold;
 }
-
-
-
+.color-box{
+  width:40px;
+  height:40px;
+  display: inline-block;
+  margin: 1em 0.5em 0 0em ;
+  border: 1px solid grey;
+}
+.h-flex{
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.v-flex{
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
 </style>
